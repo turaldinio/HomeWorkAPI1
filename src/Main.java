fc62e48c7098cdde63598f99337781e1f2ea4340
Tural Guluev <tural-98@bk.ru>
2022-05-27 18:12:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+        for (int a : intList) {
+            if (a > 0 && a % 2 == 0) {
+                result.add(a);
+            }
+        }
+        Collections.sort(result);
+        System.out.println(result);
+    }
+}
